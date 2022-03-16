3a071c4ae7ec21773992c91f981268563e049f8d
Nayran Ziebell <nayran@Nayrans-MacBook-Pro.local>
2022-03-16 17:46:14 -0300

    Scrumdinger

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nayran Ziebell on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
